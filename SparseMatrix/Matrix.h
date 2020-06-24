de320564f311b3dc13716b0312699b471fea3b62
Sophia Shuvar <45092554+sophiashuv@users.noreply.github.com>
2020-06-24 15:08:07 +0300

    Update Matrix.h

--- SparseMatrix/Matrix.h
+++ SparseMatrix/Matrix.h
@@ -13,6 +13,8 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <type_traits>
+
 
 using namespace std;
 
@@ -63,8 +65,8 @@ private:
     size_t y_length = 0;
 
 public:
-    void readMatrix(string file_name) {
-        size_t x = 0, y= 0;
+    void readMatrix(const string &file_name) {
+        size_t x = 0, y = 0;
         ifstream myfile(file_name);
         if (myfile.is_open()) {
             string c;
@@ -118,15 +120,14 @@ public:
         if (x >= x_length) throw "Index out of range";
         if (y >= y_length) throw "Index out of range";
         size_t s = this->matrix.size();
-        for (size_t i = 0; i < s; i++){
+        for (size_t i = 0; i < s; i++)
             if (this->matrix[i].x == x && this->matrix[i].y == y) {
                 this->matrix.erase(this->matrix.begin() + i);
                 break;
             }
-        }
     }
 
-    Matrix operator +(Matrix matrix) {
+    Matrix operator +(const Matrix &matrix) {
         if (matrix.x_length == x_length && matrix.y_length == y_length) {
             Matrix result;
             result.x_length = x_length;
@@ -176,7 +177,7 @@ public:
         } else throw "Matrix size exception!";
     }
 
-    Matrix operator *(int a) {
+    Matrix operator *(const int &a) {
         Matrix result;
         result.x_length = x_length;
         result.y_length = y_length;
@@ -185,57 +186,33 @@ public:
         return result;
     }
 
-//    Matrix operator *(Matrix matrix) {
-//        if (matrix.y_length == x_length && matrix.x_length == y_length)
-//        {
-//            Matrix result;
-//            result.x_length = matrix.x_length;
-//            result.y_length = y_length;
-//            vector<Node<T>> temp_vector;
-//            int temp_value = 0;
-//            for (size_t i = 0; i < this->matrix.size(); i++)
-//            {
-//                for (size_t j = 0; j < matrix.matrix.size(); j++)
-//                {
-//                    if (this->matrix[i].y == matrix.matrix[j].x)
-//                    {
-//                        temp_value = this->matrix[i].data * matrix.matrix[j].data;
-//                        if (temp_value != 0)
-//                        {
-//                            temp_vector.push_back(Node<T>(temp_value, this->matrix[i].x, matrix.matrix[j].y));
-//                        }
-//                    }
-//                }
-//            }
-//            for (size_t i = 0; i < temp_vector.size(); i++)
-//            {
-//                for (size_t j = 0; j < temp_vector.size(); j++)
-//                {
-//                    if (i != j)
-//                    {
-//                        if (temp_vector[i].x == temp_vector[j].x && temp_vector[i].y == temp_vector[j].y)
-//                        {
-//                            temp_vector[i].data += temp_vector[j].data;
-//                            temp_vector[j].x = x_length + i + 1;
-//                        }
-//                    }
-//                }
-//            }
-//            for (size_t i = 0; i < temp_vector.size(); i++)
-//            {
-//                if (temp_vector[i].x < x_length)
-//                {
-//                    result.matrix.push_back(temp_vector[i]);
-//                }
-//            }
-//            return result;
-//        } else throw "Matrix size exception!";
-//    }
+    Matrix operator *(const Matrix &matrix) {
+        if (matrix.y_length == x_length && matrix.x_length == y_length) {
+            Matrix result;
+            result.x_length = matrix.x_length;
+            result.y_length = y_length;
+            vector<Node<T>> temp_vector;
+            int temp_value = 0;
+            for (size_t i = 0; i < this->matrix.size(); i++)
+                for (size_t j = 0; j < matrix.matrix.size(); j++)
+                    if (this->matrix[i].y == matrix.matrix[j].x) {
+                        temp_value = this->matrix[i].data * matrix.matrix[j].data;
+                        if (temp_value != 0) temp_vector.push_back(Node<T>(temp_value, this->matrix[i].x, matrix.matrix[j].y));
+                    }
+            for (size_t i = 0; i < temp_vector.size(); i++)
+                for (size_t j = 0; j < temp_vector.size(); j++)
+                    if (i != j)
+                        if (temp_vector[i].x == temp_vector[j].x && temp_vector[i].y == temp_vector[j].y) {
+                            temp_vector[i].data += temp_vector[j].data;
+                            temp_vector[j].x = x_length + i + 1;
+                        }
+            for (size_t i = 0; i < temp_vector.size(); i++) if (temp_vector[i].x < x_length) result.matrix.push_back(temp_vector[i]);
+            return result;
+        } else throw "Matrix size exception!";
+    }
 
-    T check(int i, int j) {
-        for (size_t k = 0; k < matrix.size(); k++) {
-            if (matrix[k].x == i && matrix[k].y == j) return matrix[k].data;
-        }
+    T check(size_t i, size_t j) {
+        for (size_t k = 0; k < matrix.size(); k++) if (matrix[k].x == i && matrix[k].y == j) return matrix[k].data;
         return T();
     }
 
@@ -243,8 +220,8 @@ public:
         string res = "";
         for (size_t i = 0; i < x_length; i++) {
             for (size_t j = 0; j < y_length; j++) {
-                if (is_same<char*, decltype(*this)>::value) res += check(i, j)+ ' ';
-                else res += to_string(check(i, j)) + ' ';
+                T ch = check(i, j);
+                res += to_string(ch) + ' ';
             }
             res += "\n";
         } return res;
